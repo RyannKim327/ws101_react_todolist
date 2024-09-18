53df5f407fb43bbe3d51f3c2733ba56a0447d624
RyannKim327 <rksesgundo123@gmail.com>
2024-09-18 21:41:21 +0800

    09-18-24 21:41

---
+++ src/components/field.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import "./../stylesheets/field.css";
+
+class Field extends Component {
+  render() {
+    return (
+      <div className="float">
+        <form>
+          <h1>Todo Form</h1>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Field;
